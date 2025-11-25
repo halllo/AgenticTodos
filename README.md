78f54536a6712cd7ad3605f7ed022a24dd5dfbda
Manuel Naujoks <mnaujoks@gmail.com>
2025-11-25 19:39:33 +0100

    using amazon bedrock -> "Extra inputs are not permitted"

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This experimental application aims to explore the following technologies:
 - Microsoft Agent Framework
 - AG-UI
 - WebMCP
+
+## Problems
+
+Amazon Bedrock Runtime client throws this exception, when used with AG-UI:
+
+```log
+Amazon.BedrockRuntime.Model.ValidationException: The model returned the following errors: ag_ui_thread_id: Extra inputs are not permitted
+---> Amazon.Runtime.Internal.HttpErrorResponseException: Exception of type 'Amazon.Runtime.Internal.HttpErrorResponseException' was thrown.
+at Amazon.Runtime.HttpWebRequestMessage.ProcessHttpResponseMessage(HttpResponseMessage responseMessage)
+```
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using Amazon.BedrockRuntime;
 using Microsoft.Agents.AI;
 using Microsoft.Agents.AI.Hosting.AGUI.AspNetCore;
 using Microsoft.Extensions.AI;
@@ -17,12 +18,15 @@ var app = builder.Build();
 app.MapOpenApi();
 app.MapScalarApiReference();
 app.MapGet("/", () => "Hello World!");
-app.MapAGUI("/agui", CreateAgent(builder));
+app.MapAGUI("/agui", 
+    //CreateOpenAIAgent(builder)
+    CreateAmazonBedrockAgent(builder)
+);
 
 app.Run();
 
 
-static AIAgent CreateAgent(WebApplicationBuilder builder)
+static AIAgent CreateOpenAIAgent(WebApplicationBuilder builder)
 {
     var agent = new OpenAIClient(builder.Configuration["OPENAI_API_KEY"] ?? throw new InvalidOperationException("OPENAI_API_KEY is not set."))
        .GetChatClient("gpt-4o")
@@ -31,6 +35,27 @@ static AIAgent CreateAgent(WebApplicationBuilder builder)
            name: "AGUIAssistant",
            tools: [
                AIFunctionFactory.Create(
+                () => DateTimeOffset.UtcNow,
+                name: "get_current_time",
+                description: "Get the current UTC time."
+            )
+           ]);
+    return agent;
+}
+
+static AIAgent CreateAmazonBedrockAgent(WebApplicationBuilder builder)
+{
+    var runtime = new AmazonBedrockRuntimeClient(
+        awsAccessKeyId: builder.Configuration["AWSBedrockAccessKeyId"]!,
+        awsSecretAccessKey: builder.Configuration["AWSBedrockSecretAccessKey"]!,
+        region: Amazon.RegionEndpoint.GetBySystemName(builder.Configuration["AWSBedrockRegion"]!));
+
+    var agent = runtime
+        .AsIChatClient("eu.anthropic.claude-sonnet-4-20250514-v1:0")
+        .CreateAIAgent(
+           name: "AGUIAssistant",
+           tools: [
+            AIFunctionFactory.Create(
                 () => DateTimeOffset.UtcNow,
                 name: "get_current_time",
                 description: "Get the current UTC time."
