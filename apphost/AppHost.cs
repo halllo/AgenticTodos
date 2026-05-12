dd0457bdd65da64a284127d0424dac306ae3ccc5
Manuel Naujoks <mnaujoks@gmail.com>
2026-05-12 10:47:35 +0200

    Refactor AppHost setup to improve structure and add Azure Container App environment

--- apphost/AppHost.cs
+++ apphost/AppHost.cs
@@ -1,15 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mcpserver = builder.AddProject<Projects.AgenticTodos_McpServer>("AgenticTodos-McpServer");
+builder.AddAzureContainerAppEnvironment("agentic-todos");
+
+var mcpserver = builder.AddProject<Projects.AgenticTodos_McpServer>("AgenticTodos-McpServer")
+    .WithExternalHttpEndpoints()
+    ;
 
 var backend = builder.AddProject<Projects.AgenticTodos_Backend>("AgenticTodos-Backend")
-    .WithReference(mcpserver);
+    .WithReference(mcpserver)
+    .WithExternalHttpEndpoints()
+    ;
 
 var element = builder.AddViteApp("AgenticTodos-Frontend", "../frontend")
     .WithEndpoint("http", (endpointAnnotation) =>
     {
         endpointAnnotation.Port = 3000;
     })
-    .WithReference(backend);
+    .WithReference(backend)
+    .WithExternalHttpEndpoints()
+    ;
 
 builder.Build().Run();
